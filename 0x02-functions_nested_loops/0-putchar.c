ffe1c7cf609ecfdceab97f461ac7b783cff62575
simoonge1 <simoimagine@gmail.com>
2023-07-18 11:08:50 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,33 +1,18 @@
- 
-
- /*
- * File: 0-putchar.c 
- *
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
  */
+int main(void)
+{
+	char *sh = "_putchar";
 
- #include "main.h" 
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
 
- /** 
- * main-print putchar followed by a new line
- * 
- *Return :always 0
- */
-  int main(void) 
- {
-      _putchar('_' );
-      _putchar ('p' );
-      _putchar ('u' );
-      _putchar ('t' );
-      _putchar ('c' );
-      _putchar ('h' );
-      _putchar ('a' );
-      _putchar ('r' );
-      _putchar ('\n');
-      
-       return (0) ;
-} 
-        
-
-
-
-
+	return (0);
+}
