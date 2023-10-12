1ac26de97a9f1cc33ac63d89e21d6f5955eff821
simoonge1 <simoimagine@gmail.com>
2023-10-12 03:22:44 -0700

    c

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -1,15 +1,12 @@
 #!/user/bin/python3
 
-def largest_palindrome():
-  largest_palindrome = 0
-  for i in range(100, 1000):
+largest_palindrom = 0
+for i in range(100, 1000):
     for j in range(100, 1000):
-      product = i * j
-      if str(product) == ''.join(reversed(str(product))) and product > largest_palindrome:
-        largest_palindrome = product
-  return largest_palindrome
+          product = i * j
+          if str(product) == str(product)(::-1) and product > largest_palindrome:
+              largest_palindrome = product
 
-
-# save the result in the file
-with open("102-result", "w") as f:
-  f.write(str(largest_palindrome()))
+#save the result in the file
+whit open("102-result","w") as file:
+  file.write(str(largest_palindrome))
